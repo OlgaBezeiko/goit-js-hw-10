e5d866c0fe731d47243ed34565d62f4fac22784f
OlgaBezeiko <122458798+OlgaBezeiko@users.noreply.github.com>
2023-06-03 21:55:13 +0300

    03/06/23 6

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,18 +1,18 @@
 // cat-api.js
 
-const apiKey = 'live_K50XRdXZxBUocJl4iOWUECHUubPo46LLcOKHPW8OdNA71QPbUeJ0Xj4kEvl5qHDK';
+const API_KEY = 'live_K50XRdXZxBUocJl4iOWUECHUubPo46LLcOKHPW8OdNA71QPbUeJ0Xj4kEvl5qHDK';
 
-export function fetchBreeds() {
-  const url = 'https://api.thecatapi.com/v1/breeds';
+export function fetchCatByBreed(breedId) {
+  const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
 
   return fetch(url, {
     headers: {
-      'x-api-key': apiKey
+      'x-api-key': API_KEY
     }
   })
     .then(response => {
       if (!response.ok) {
-        throw new Error('Failed to fetch breeds');
+        throw new Error('Failed to fetch cat by breed');
       }
       return response.json();
     });
--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-import { fetchBreeds } from './cat-api.js';
+import { fetchCatByBreed } from './cat-api.js';
 
-fetchBreeds()
-  .then(breeds => {
-    // Використовуйте отриманий масив порід тут
-    console.log(breeds);
-  })
-  .catch(error => {
-    // Обробка помилки
-    console.error(error);
-  });
+// Отримання значення вибраної опції зі селекту
+const selectElement = document.querySelector('.breed-select');
+selectElement.addEventListener('change', (event) => {
+  const selectedBreedId = event.target.value;
 
-  
+  fetchCatByBreed(selectedBreedId)
+    .then(cat => {
+      // Використати дані про кота тут
+      console.log(cat);
+      displayCatInfo(cat);
+    })
+    .catch(error => {
+      // Обробка помилки
+      console.error(error);
+    });
+});
+
+function displayCatInfo(cat) {
+  const catInfoContainer = document.querySelector('.cat-info');
+  catInfoContainer.innerHTML = `
+    <img src="${cat[0].url}" alt="Cat Image">
+    <h2>${cat[0].breeds[0].name}</h2>
+    <p>Description: ${cat[0].breeds[0].description}</p>
+    <p>Temperament: ${cat[0].breeds[0].temperament}</p>
+  `;
+}
