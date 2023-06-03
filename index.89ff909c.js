7bf221ffd2767e7ea8fd9a10676a4f723a96d8d5
OlgaBezeiko <122458798+OlgaBezeiko@users.noreply.github.com>
2023-06-03 18:55:40 +0000

    Deploying to gh-pages from @ OlgaBezeiko/goit-js-hw-10@e5d866c0fe731d47243ed34565d62f4fac22784f 🚀

--- index.89ff909c.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.thecatapi.com/v1/breeds",{headers:{"x-api-key":"live_K50XRdXZxBUocJl4iOWUECHUubPo46LLcOKHPW8OdNA71QPbUeJ0Xj4kEvl5qHDK"}}).then((e=>{if(!e.ok)throw new Error("Failed to fetch breeds");return e.json()})).then((e=>{console.log(e)})).catch((e=>{console.error(e)}));
-//# sourceMappingURL=index.89ff909c.js.map
